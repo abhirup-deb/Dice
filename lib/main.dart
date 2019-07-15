e9c4958fb126801351fe8cfde8f920559db47dc8
abhirup-deb <debabhirup1999@gmail.com>
2019-07-15 21:26:06 +0530

    Random Func. Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -23,6 +24,7 @@ class DicePage extends StatefulWidget {
 
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -32,7 +34,7 @@ class _DicePageState extends State<DicePage> {
           child: FlatButton(
           onPressed: () {
             setState(() {
-              leftDiceNumber = 3;
+              leftDiceNumber = Random().nextInt(6)+1;
               print('diceNumber = $leftDiceNumber');
             });
           print('Button Clicked');
@@ -43,12 +45,14 @@ class _DicePageState extends State<DicePage> {
         Expanded(
           child: FlatButton(
           onPressed: () {
-          print('Button Clicked');
+            setState(() {
+              rightDiceNumber = Random().nextInt(6)+1;
+              print('diceNumber = $rightDiceNumber');
+            });
           },
-          child: Image.asset('images/dice1.png'),
+          child: Image.asset('images/dice$rightDiceNumber.png'),
           ),
         ),
-
         ],
       ),
     );
