168c4bc086b2552de7322d6712716eb77c121a7a
abhirup-deb <debabhirup1999@gmail.com>
2019-07-15 21:16:11 +0530

    State Change Func. Added

--- lib/main.dart
+++ lib/main.dart
@@ -16,28 +16,38 @@ void main() {
   
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                print('Button Clicked');
-              },
-              child: Image.asset('images/dice1.png'),
-            ),
+        Expanded(
+          child: FlatButton(
+          onPressed: () {
+            setState(() {
+              leftDiceNumber = 3;
+              print('diceNumber = $leftDiceNumber');
+            });
+          print('Button Clicked');
+          },
+          child: Image.asset('images/dice$leftDiceNumber.png'),
           ),
-          Expanded(
-            child: FlatButton(
-                onPressed: () {
-                print('Button Clicked');
-              },
-              child: Image.asset('images/dice1.png'),
-            ),
+        ),
+        Expanded(
+          child: FlatButton(
+          onPressed: () {
+          print('Button Clicked');
+          },
+          child: Image.asset('images/dice1.png'),
           ),
+        ),
 
         ],
       ),
